a46eaf5786a5be4d59d5647b245452f23d952b30
LAPTOP-QSBIPBFB\김재윤 <kimjy9698@naver.com>
2022-08-02 18:47:11 +0900

    [김재윤]
    Test 문제 2022-08-02

---
+++ src/test/java/com/kimjy/test/SpringExampletestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimjy.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampletestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
